4e0b05db9ca47e5809ada0f9dfde6e7770feda3b
mazdato <toshihide.matsuda@gmail.com>
2022-10-16 23:35:06 +0900

    release build

--- AfterImage/AdsConst.swift
+++ AfterImage/AdsConst.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-fileprivate let debug = true;
+fileprivate let debug = false;
 
 public func bannerViewId() -> String{
     return debug ? "ca-app-pub-3940256099942544/2934735716" : "ca-app-pub-1643629923616505/4567849365";
