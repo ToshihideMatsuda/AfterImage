f905ce382191f8d418248edcc68902dbbe797a8a
mazdato <toshihide.matsuda@gmail.com>
2022-10-21 14:16:35 +0900

    評価の表示

--- AfterImage/ViewController.swift
+++ AfterImage/ViewController.swift
@@ -63,6 +63,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         requestAppStoreReview()
     }
 
