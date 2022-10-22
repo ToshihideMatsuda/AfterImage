8440330347b24eaf6bec70bb862b63af81bfa107
mazdato <toshihide.matsuda@gmail.com>
2022-10-23 02:03:31 +0900

    update

---
+++ AppStore/ffmpeg.sh
@@ -0,0 +1,4 @@
+ffmpeg -i 886.mp4 -c:v libx264 -profile:v baseline -level:v 3.1 -c:a copy -r 30 output_886.mp4
+
+
+ffmpeg -i 4-1-2.mp4  -c:v libx264 -profile:v baseline -level:v 3.1 -c:a copy -r 30  4-1-2-output.mp4
